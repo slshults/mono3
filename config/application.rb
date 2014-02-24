969ff15d66580d8824c011da2d4a8e4370b38a07
unknown <steven@shults.org>
2014-02-23 19:30:51 -0800

    no joy redirecting the evil, so testing to see if rack-rewrite will at least add the www if someone tries to get in without it.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
 config.middleware.insert 0, 'Rack::Rewrite' do
-  r301 %r{.*}, 'http://demosites.conversionsupport.com$&',
+  r301 %r{.*}, 'http://shakespeare-monologues.org$&',
   :if => Proc.new { |rack_env| rack_env['SERVER_NAME'] != 'www.shakespeare-monologues.org' }
 end
