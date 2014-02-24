f5b3b3de49af244560fd36deab87872902352dd8
unknown <steven@shults.org>
2014-02-23 16:59:43 -0800

    slight tweak to the previous

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-config.middleware.insert 0, 'Rack::Rewrite' {} do
+config.middleware.insert 0, 'Rack::Rewrite' do
   r301 %r{.*}, 'http://canonical-domain.com$&',
   :if => Proc.new { |rack_env| rack_env['SERVER_NAME'] != 'www.shakespeare-monologues.org' }
 end
