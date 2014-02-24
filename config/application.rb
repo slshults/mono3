82e70324a2bf19be59fb29133db66b59111af064
unknown <steven@shults.org>
2014-02-23 19:32:47 -0800

    removing applications.rb which contained nothing but a rack-rewrite rule which didn't precisely jackshit, as expected

--- config/application.rb
+++
@@ -1,4 +0,0 @@
-config.middleware.insert 0, 'Rack::Rewrite' do
-  r301 %r{.*}, 'http://shakespeare-monologues.org$&',
-  :if => Proc.new { |rack_env| rack_env['SERVER_NAME'] != 'www.shakespeare-monologues.org' }
-end
