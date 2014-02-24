cc58ab418e3815790c88b491819b5f287f764122
unknown <steven@shults.org>
2014-02-23 19:27:31 -0800

    no joy. trying adding to top of stack.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+config.middleware.insert 0, 'Rack::Rewrite' do
   r301 %r{.*}, 'http://demosites.conversionsupport.com$&',
   :if => Proc.new { |rack_env| rack_env['SERVER_NAME'] != 'www.shakespeare-monologues.org' }
 end
