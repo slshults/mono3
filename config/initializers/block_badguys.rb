7c40e640e2901f5c0d735514d73e48b179ef5493
unknown <steven@shults.org>
2014-02-25 20:35:28 -0800

    logs don't show that anything actually happened there, so trying again

--- config/initializers/block_badguys.rb
+++ config/initializers/block_badguys.rb
@@ -2,8 +2,7 @@ ShakespeareMonologues::Application.configure do
 
   config.middleware.insert_before(Rack::Lock, Rack::Block) do
     ip_pattern '8.35.201.' do
-      # expressions like '192.0.0.' also available
-        halt 404
-      end
+      halt 404
+     end
   end
 end
