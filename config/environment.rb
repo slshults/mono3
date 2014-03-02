d4aafc8a018d7191e1d793742bc47d0a78bc2a64
unknown <steven@shults.org>
2014-03-01 16:09:20 -0800

    upgrading from rails 2.3.15 to 2.3.16

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,7 @@ Rails::Initializer.run do |config|
 
 end
 
+
 class Logger
   def format_message(severity, timestamp, progname, msg)
     "[#{timestamp.strftime("%Y-%m-%d %H:%M:%S")}] #{severity} #{msg}\n"
