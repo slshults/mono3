e1094a7b5b59258c99619f18c0e7e9bae9390242
unknown <steven@shults.org>
2014-07-07 19:21:00 -0700

    added a links page, but not done yet so no links point to it. Added a link to the android app from the addtohomescreen tip

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -12,6 +12,10 @@ class StaticController < ApplicationController
      render :file => 'static/aboutus.html'
    end
 
+   def links
+     render :file => 'static/links.html'
+   end
+
    def privacy
      render :file => 'static/privacy.html'
    end
